d109a5caed574db5a8e8846138c8cd7e8e1515f9
Marcelo Mira <hello@marcemira.com>
2021-09-08 23:30:51 -0300

    [FEATURE] Adds support to optionally migrate directus tables

--- src/schema/read-schema.ts
+++ src/schema/read-schema.ts
@@ -7,13 +7,16 @@ import log from "../utils/logger";
 /**
  * Reads the current schema of the database
  */
-export default async function readSchema(): Promise<SchemaState> {
+export default async function readSchema(allowedDirectusTables: string): Promise<SchemaState> {
   log.message("loading", "Reading your database schema");
 
   const inspector = SchemaInspector(db());
+  const allowedTables = allowedDirectusTables.length ? allowedDirectusTables.split(',') : [];
 
   // Directus will take care of the system tables, so we exclude them
-  const columnDefs = (await inspector.columnInfo()).filter(col => !isDirectusTable(col.table));
+  const columnDefs = (await inspector.columnInfo()).filter(col =>
+    allowedDirectusTables.includes(col.table) || !isDirectusTable(col.table)
+  );
 
   const state = columnDefs.reduce((result, def) => {
     const table = result.find(table => table.name === def.table);
